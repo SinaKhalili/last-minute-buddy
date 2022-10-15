a3ca7694c252cee024b754d9e776aa75ce3591e4
Wing Lee <44917547+wingleeio@users.noreply.github.com>
2022-10-15 15:37:40 -0700

    add chakra remove stiches cause wing vetoed stiches

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,20 +1,15 @@
-import { Head, Html, Main, NextScript } from 'next/document'
-
-import { getCssText } from '../stitches.config'
+import { Head, Html, Main, NextScript } from "next/document";
 
 export default function MyDocument() {
   return (
     <Html lang="en">
       <Head>
-        <style
-          id="stitches"
-          dangerouslySetInnerHTML={{ __html: getCssText() }}
-        />
+        <title>Last Minute Buddy</title>
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,43 +1,9 @@
-import { Session } from "@supabase/supabase-js";
-import Head from "next/head";
 import { useEffect, useState } from "react";
-import StitchesLogo from "../components/StitchesLogo";
+
 import Account from "../modules/auth/components/Account";
 import Auth from "../modules/auth/components/Auth";
+import { Session } from "@supabase/supabase-js";
 import { supabase } from "../modules/database/supabase";
-import { styled } from "../stitches.config";
-
-const Box = styled("div", {});
-
-const Text = styled("p", {
-  fontFamily: "$system",
-  color: "$hiContrast",
-});
-
-const Link = styled("a", {
-  fontFamily: "$system",
-  textDecoration: "none",
-  color: "$emerald700",
-});
-
-const Container = styled("div", {
-  marginX: "auto",
-  paddingX: "$3",
-
-  variants: {
-    size: {
-      1: {
-        maxWidth: "300px",
-      },
-      2: {
-        maxWidth: "585px",
-      },
-      3: {
-        maxWidth: "865px",
-      },
-    },
-  },
-});
 
 export default function Home() {
   const [isLoading, setIsLoading] = useState(true);
