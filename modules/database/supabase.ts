a9313e787dff4b05dac5dfac4e6e013fefa0c041
sina <khalili@sfu.ca>
2022-10-15 15:16:20 -0700

    Update supabase API keys

--- modules/database/supabase.ts
+++ modules/database/supabase.ts
@@ -1,11 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
-import { ensureEnvironmentVariables } from "../common/util/ensureEnvironmentVariables";
 
-const { NEXT_PUBLIC_SUPABASE_URL, NEXT_PUBLIC_SUPABASE_ANON_KEY } =
-  ensureEnvironmentVariables([
-    "NEXT_PUBLIC_SUPABASE_URL",
-    "NEXT_PUBLIC_SUPABASE_ANON_KEY",
-  ]);
+let NEXT_PUBLIC_SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL ?? "";
+let NEXT_PUBLIC_SUPABASE_ANON_KEY =
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? "";
 
 export const supabase = createClient(
   NEXT_PUBLIC_SUPABASE_URL,
